e7a75fa4fe11ea5b885f1c1ae23b77248f146829
milivojevic85 <misagrf@yahoo.com>
2020-01-25 23:22:41 +0100

    constructor

--- shop.php
+++ shop.php
@@ -8,8 +8,25 @@
 <?php 
 class ShopProduct 
 {
+	public $title = "default product";
+	public $producerMainName = "main name";
+	public $producerFirstName = "first name";
+	public $price = 0;
 	
+	public function __construct($title, $firstName, $mainName, $price) {
+		$this->title = $title;
+		$this->producerFirstName = $firstName;
+		$this->producerMainName = $mainName;
+		$this->price = $price;
+	}
+	
+	public function getProducer() {
+		return $this->producerFirstName." ".$this->producerMainName."<br>";
+	}
 }
+
+$product1 = new ShopProduct("My Antonia", "Willa", "Cather", 5.99);
+echo $product1->getProducer()."<br>"; // Willa Cather
 ?>
 </body>
 </html>
